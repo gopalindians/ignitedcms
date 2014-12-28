5379eea44e9b9df708e80aede1dfed11f82406b8
ignitedcms <kungfuthemes@hotmail.com>
2014-12-28 01:33:22 +0000

    menu refined

--- application/views/sitepreview/menu copy.php
+++ application/views/sitepreview/menu copy.php
@@ -3,43 +3,7 @@
         <nav> 
 
         <ul class="sf-menu" id="example">
-            <li >
-                <a href="<?php echo site_url('landing/homepage'); ?>" >Home</a>
-                
-            </li>
-            <li>
-                <a href="<?php echo site_url('landing/this_is_alpha'); ?>" >This is AlphaPFM <i class="fa fa-chevron-down"></i></a>
-                <ul>
-                    <li>
-                        <a href="<?php echo site_url('landing/benefits'); ?>" >5 Key Benefits</a>
-                    </li>
-                    <li >
-                        <a href="<?php echo site_url('landing/which_package'); ?>">Which package is right</a>
-                    </li>  
-                </ul>
-            </li>
             
-          
-             
-            <li>
-                <a href="<?php echo site_url('landing/blog'); ?>" >Blog</a>
-            </li>
-            <li>
-                <a href="<?php echo site_url('forum/get_topics'); ?>" >Forums</a>
-            </li>
-            <li>
-                <a href="<?php echo site_url('landing/faq'); ?>" >FAQ</a>
-            </li>
-             <li>
-                <a href="<?php echo site_url('landing/contact_us'); ?>" >Contact Us</a>
-            </li>
-
-
-            <li>
-             <div> <a href="<?php echo site_url('login'); ?>" class="stop btn btn-purplet btn-xs" style="padding:10px;"><i class="fa fa-user"></i> Login </a></div>
-               
-
-            </li>
         </ul> <!--end class sf-menu-->
     
        <div class="clearfix"></div>
@@ -47,17 +11,8 @@
           <!-- <div class="small-menu" style="float:left; position:relative;">
           <select onchange="location = this.options[this.selectedIndex].value;">
               <option selected="selected" value="">Go to...</option>
-              <option value="<?php echo site_url('landing/homepage'); ?>">Home</option>
-              <option value="<?php echo site_url('landing/this_is_alpha'); ?>">This is AlphaPFM</option>
-              <option value="<?php echo site_url('landing/benefits'); ?>">--Five Key Benefits</option>
-              <option value="<?php echo site_url('landing/which_package'); ?>">--Which Package is Right</option>
-             
-              <option value="<?php echo site_url('shop/shop_front'); ?>">Shop</option>
-            
+              <option value="<?php echo site_url('xxx'); ?>">Home</option>
               
-              <option value="<?php echo site_url('landing/blog'); ?>">Blog</option>
-              <option value="<?php echo site_url('forum/get_topics'); ?>">Forums</option>
-              <option value="<?php echo site_url('login'); ?>">Login</option>
           </select>
           </div> -->
       
